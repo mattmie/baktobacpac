ceb0a35fa120816fb23397448d8d2145746db310
bstets1 <52285717+bstets1@users.noreply.github.com>
2022-10-13 10:03:27 -0400

    Update create-bacpacs.sh

--- create-bacpacs.sh
+++ create-bacpacs.sh
@@ -17,7 +17,7 @@ done
 /opt/mssql-tools/bin/sqlcmd -l 300 -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i "/create_procedure_restoredatabase.sql"
 #run the setup script to create the DB and the schema in the DB
 #do this in a loop because the timing for when the SQL instance is ready is indeterminate
-for f in /mnt/external/$env_names.bak;
+for f in /mnt/external/$names.bak;
 do
     s=${f##*/}
     name="${s%.*}"
